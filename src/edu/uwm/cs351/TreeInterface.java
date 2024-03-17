c52a8af17a556c53296860476d09ec0d953882f3
Satish7490 <jsatish053@gmail.com>
2024-03-17 13:02:58 -0500

    Created a Interface that has common methods for AVL tree and ZIP Tree

---
+++ src/edu/uwm/cs351/TreeInterface.java
@@ -0,0 +1,23 @@
+package edu.uwm.cs351;
+
+public interface TreeInterface<T extends Comparable<T>> {
+	
+	// Method to insert a new node with the given key
+    void insert(T key);
+
+    // Method to delete a node with the given key
+    void delete(T key);
+
+    // Method to search for a node with the given key
+    boolean search(T key);
+
+    // Method to check if the tree is empty
+    boolean isEmpty();
+
+    // Method to get the height of the tree
+    int getHeight();
+
+    // Method to get the size (number of nodes) of the tree
+    int getSize();
+
+}
