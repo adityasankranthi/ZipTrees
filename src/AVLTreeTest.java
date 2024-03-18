b3709780fe56551cc35034575245a885400e8983
Satish7490 <jsatish053@gmail.com>
2024-03-17 19:28:43 -0500

    Updated Insert Method, Added Junit test file

---
+++ src/AVLTreeTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.uwm.cs351.AVLTree;
+
+
+
+public class AVLTreeTest {
+
+	public void testInsert() {
+        AVLTree<Integer> tree = new AVLTree<>();
+
+
+}
+}
--- src/edu/uwm/cs351/AVLTree.java
+++ src/edu/uwm/cs351/AVLTree.java
@@ -24,10 +24,94 @@ public class AVLTree<T extends Comparable<T>> implements TreeInterface<T> {
 	}
 
 	@Override
-	public void insert(T key) {
-		// TODO Auto-generated method stub
+    public void insert(T key) {
+        root = insert(root, key);
+    }
 
-	}
+    private AVLNode<T> insert(AVLNode<T> node, T key) {
+        if (node == null) {
+            return new AVLNode<>(key);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = insert(node.left, key);
+        } else if (cmp > 0) {
+            node.right = insert(node.right, key);
+        }
+
+    
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+
+       
+        int balance = getBalance(node);
+
+        //  4 cases
+
+        // Left Left Case
+        if (balance > 1 && key.compareTo(node.left.key) < 0) {
+            return rotateRight(node);
+        }
+
+        // Right Right Case
+        if (balance < -1 && key.compareTo(node.right.key) > 0) {
+            return rotateLeft(node);
+        }
+
+        // Left Right Case
+        if (balance > 1 && key.compareTo(node.left.key) > 0) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        // Right Left Case
+        if (balance < -1 && key.compareTo(node.right.key) < 0) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+        return node;
+    }
+
+    private int height(AVLNode<T> node) {
+        return node == null ? 0 : node.height;
+    }
+    
+    private int getBalance(AVLNode<T> node) {
+        return node == null ? 0 : height(node.left) - height(node.right);
+    }
+
+    private AVLNode<T> rotateRight(AVLNode<T> y) {
+        AVLNode<T> x = y.left;
+        AVLNode<T> T2 = x.right;
+
+        // Perform rotation
+        x.right = y;
+        y.left = T2;
+
+        // Update heights
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+        // Return new root
+        return x;
+    }
+
+    private AVLNode<T> rotateLeft(AVLNode<T> x) {
+        AVLNode<T> y = x.right;
+        AVLNode<T> T2 = y.left;
+
+        // Perform rotation
+        y.left = x;
+        x.right = T2;
+
+        // Update heights
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+        // Return new root
+        return y;
+    }
+
 
 	@Override
 	public void delete(T key) {
