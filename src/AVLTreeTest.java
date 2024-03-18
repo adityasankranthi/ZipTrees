56c65788c0acd6fa93f6841317de3a60642c169a
Satish7490 <jsatish053@gmail.com>
2024-03-17 19:58:55 -0500

    Added search test case in AVL Test

--- src/AVLTreeTest.java
+++ src/AVLTreeTest.java
@@ -6,7 +6,7 @@ import edu.uwm.cs351.AVLTree;
 
 public class AVLTreeTest {
 
-    @Test
+	@Test
     public void testInsert() {
         AVLTree<Integer> tree = new AVLTree<>();
         tree.insert(5);
@@ -14,4 +14,18 @@ public class AVLTreeTest {
         tree.insert(15);
         assertEquals(3, tree.getSize());
     }
+	@Test
+    public void testSearch() {
+        AVLTree<Integer> tree = new AVLTree<>();
+        tree.insert(5);
+        tree.insert(10);
+        tree.insert(15);
+
+        assertTrue(tree.search(5));
+        assertTrue(tree.search(10));
+        assertTrue(tree.search(15));
+        assertFalse(tree.search(20)); // Not inserted
+    }
+    
+    
 }
